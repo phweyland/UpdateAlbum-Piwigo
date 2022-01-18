c73e47eae21a31c6b779a021206ce899670e2ad0
phweyland <philippe.weyland@laposte.net>
2022-01-18 10:14:19 -0300

    add Slovenian language (thanks to maxima from Piwigo.org)

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * as much as possible prefer using comprehensive keys (such as "Check here") instead of
+ * formatted keys, it would make your code more readable and help
+ * the translation team as well
+ */
+
+$lang['Update Album'] = 'Posodobite album';
+$lang['Update existing images'] = 'Posodobite obstoječe slike';
+$lang['Create images if not exist'] = 'Ustvarite slike, če ne obstajajo';
+$lang['Select Album to be updated'] = 'Izberite Album, ki ga želite posodobiti';
+$lang['Update'] = 'Posodobite';
+$lang['Selected Album'] = 'Izbrani album';
+$lang['Selected Files'] = 'Izbrane datoteke';
+$lang['Updated image'] = 'Posodobljena slika';
+$lang['Not updated image'] = 'Slika ni posodobljena';
+$lang['Added image'] = 'Dodana slika';
+$lang['Not added image'] = 'Slika ni dodana';
+$lang['Display detailed actions done'] = 'Prikažite podrobna opravljena dejanja';
+$lang['Updated images'] = 'Posodobljena(e) slika(e)';
+$lang['Created images'] = 'Ustvarjena(e) slika(e)';
+$lang['Error'] = 'Napaka';
+$lang['Error(s)'] = 'Napaka(e)';
+$lang['Unsupported file extension'] = 'Nepodprta pripona datoteke';
+$lang['Allowed file types'] = 'Dovoljene vrste datotek';
+$lang['No album selected'] = 'Izbran ni noben album';
+$lang['Save'] = 'Shrani';
+$lang['Manage caddie\'s photos'] = 'Upravljajte caddiejeve slike';
+$lang['No file selected'] = 'Izbrana ni nobena datoteka';
+$lang['Add new images to caddie'] = 'Dodajte nove slike v caddie';
+?>

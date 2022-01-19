a7bdcdadffb9cce1d5b979f1793258f9556fac9e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-19 11:19:21 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Update Album'] = 'Album aktualisieren';
+$lang['Update existing images'] = 'Vorhandene Bilder aktualisieren';
+$lang['Updated image'] = 'Aktualisiertes Bild';
+$lang['Updated images'] = 'Aktualisierte(s) Bild(er)';
+$lang['Selected Album'] = 'Ausgewähltes Album';
+$lang['Selected Files'] = 'Ausgewählte Datei(en)';
+$lang['Unsupported file extension'] = 'Nicht unterstützte Dateierweiterung';
+$lang['Update'] = 'Aktualisierung';
+$lang['Not added image'] = 'Nicht hinzugefügtes Bild';
+$lang['Not updated image'] = 'Nicht aktualisiertes Bild';
+$lang['Save'] = 'Speichern';
+$lang['Select Album to be updated'] = 'Zu aktualisierendes Album auswählen';
+$lang['Error(s)'] = 'Störung(en)';
+$lang['Manage caddie\'s photos'] = 'Fotos des Caddies verwalten';
+$lang['No album selected'] = 'Kein Album ausgewählt';
+$lang['No file selected'] = 'Keine Datei ausgewählt';
+$lang['Created images'] = 'Erstellte(s) Bild(er)';
+$lang['Display detailed actions done'] = 'Detaillierte durchgeführte Aktionen anzeigen';
+$lang['Error'] = 'Fehler';
+$lang['Add new images to caddie'] = 'Neue Bilder zu caddie hinzufügen';
+$lang['Added image'] = 'Bild hinzugefügt';
+$lang['Allowed file types'] = 'Erlaubte Dateitypen';
+$lang['Create images if not exist'] = 'Bilder erstellen, falls nicht vorhanden';

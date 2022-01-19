d6768e682fd4e54aed630cfb926e4ffc6492b826
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-19 18:31:30 +0100

    Update nl_NL, thanks to Pat2309

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Update existing images'] = 'Bestaande afbeeldingen bijwerken';
+$lang['Updated image'] = 'Bijgewerkte afbeelding';
+$lang['Updated images'] = 'Bijgewerkte afbeelding(en)';
+$lang['Add new images to caddie'] = 'Nieuwe afbeeldingen toevoegen aan caddy';
+$lang['Added image'] = 'Afbeelding toegevoegd';
+$lang['Allowed file types'] = 'Toegestane bestandstypen';
+$lang['Create images if not exist'] = 'Afbeeldingen maken als deze niet bestaan';
+$lang['Created images'] = 'Gemaakte afbeelding(en)';
+$lang['Display detailed actions done'] = 'Gedetailleerde acties weergeven:';
+$lang['Error'] = 'Fout';
+$lang['Error(s)'] = 'Fout(en)';
+$lang['Manage caddie\'s photos'] = 'Caddy\'s foto\'s beheren';
+$lang['No album selected'] = 'Geen album geselecteerd';
+$lang['No file selected'] = 'Geen bestand geselecteerd';
+$lang['Not added image'] = 'Geen afbeelding toegevoegd';
+$lang['Not updated image'] = 'Niet bijgewerkte afbeelding';
+$lang['Save'] = 'Bewaar';
+$lang['Select Album to be updated'] = 'Selecteer Album dat moet worden bijgewerkt';
+$lang['Selected Album'] = 'Geselecteerd album';
+$lang['Selected Files'] = 'Geselecteerd(e) bestand(en) ';
+$lang['Unsupported file extension'] = 'Niet-ondersteunde bestandsextensie';
+$lang['Update'] = 'Bijwerken';
+$lang['Update Album'] = 'Album bijwerken';

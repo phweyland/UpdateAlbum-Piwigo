60713839b17ceeb8e42f8fd98a5fd5866df2e437
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-19 11:19:29 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Update existing images'] = 'Opdater eksisterende billeder';
+$lang['Updated image'] = 'Opdateret billede';
+$lang['Updated images'] = 'Opdaterede billeder';
+$lang['Add new images to caddie'] = 'Tilføj nye billeder til kurv';
+$lang['Added image'] = 'Tilføjet billede';
+$lang['Allowed file types'] = 'Tilladte filtyper';
+$lang['Create images if not exist'] = 'Opret billeder hvis de ikke findes';
+$lang['Created images'] = 'Oprettede billeder';
+$lang['Display detailed actions done'] = 'Vis detaljerede udførte handlinger';
+$lang['Error'] = 'Fejl';
+$lang['Error(s)'] = 'Fejl';
+$lang['Manage caddie\'s photos'] = 'Håndter billeder i kurv';
+$lang['No album selected'] = 'Intet album valgt';
+$lang['No file selected'] = 'Ingen fil valgt';
+$lang['Not added image'] = 'Ikke-tilføjet billede';
+$lang['Not updated image'] = 'Ikke-opdateret billede';
+$lang['Save'] = 'Gem';
+$lang['Select Album to be updated'] = 'Vælg album der skal opdateres';
+$lang['Selected Album'] = 'Valgt album';
+$lang['Selected Files'] = 'Valgt(e) fil(er)';
+$lang['Unsupported file extension'] = 'Ikke-understøttet filudvidelse';
+$lang['Update'] = 'Opdater';
+$lang['Update Album'] = 'Opdater album';

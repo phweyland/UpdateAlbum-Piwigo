3216a5dade1f6cc4546db8ffb03468afc68aecb7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-12 06:32:32 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Not added image'] = 'Engri mynd bætt við';
+$lang['Not updated image'] = 'Ekki uppfærð mynd';
+$lang['Save'] = 'Vista';
+$lang['Select Album to be updated'] = 'Veldu albúm sem á að uppfæra';
+$lang['Selected Album'] = 'Valin albúm';
+$lang['Selected Files'] = 'Valdar skrár';
+$lang['Unsupported file extension'] = 'Óstudd skráarviðbót';
+$lang['Update'] = 'Uppfærsla';
+$lang['Update Album'] = 'Uppfæra albúm';
+$lang['Update existing images'] = 'Uppfærðu núverandi myndir';
+$lang['Updated image'] = 'Uppfærð mynd';
+$lang['Updated images'] = 'Uppfærðar myndir';
+$lang['Add new images to caddie'] = 'Bættu nýjum myndum við körfu';
+$lang['Added image'] = 'Bætti við mynd';
+$lang['Allowed file types'] = 'Leyfilegar skráargerðir';
+$lang['Create images if not exist'] = 'Stofnaðu myndir ef þær eru ekki til';
+$lang['Created images'] = 'Stofna mynd/-ir';
+$lang['Display detailed actions done'] = 'Sýna nákvæmar aðgerðir sem gerðar voru';
+$lang['Error'] = 'Villa';
+$lang['Error(s)'] = 'Villa/-ur';
+$lang['Manage caddie\'s photos'] = 'Stjórna myndum í körfu';
+$lang['No album selected'] = 'Ekkert albúm valið';
+$lang['No file selected'] = 'Engin skrá valin';

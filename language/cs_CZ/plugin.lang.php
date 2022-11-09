660d637f018ba50a76c272b82a7f37bdbbbaf197
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-09 09:48:22 +0100

    Update cs_CZ, thanks to gandycz

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Update existing images'] = 'Aktualizovat existující obrázky';
+$lang['Updated image'] = 'Aktualizovat obrázek';
+$lang['Updated images'] = 'Aktualizovat obrázky';
+$lang['Add new images to caddie'] = 'Přidat nové obrázky do košíku';
+$lang['Added image'] = 'Přidané obrázky';
+$lang['Allowed file types'] = 'Povolené typy souborů';
+$lang['Create images if not exist'] = 'Vytvořit obrázky pokud neexistují';
+$lang['Created images'] = 'Vytvořené obrázky';
+$lang['Display detailed actions done'] = 'Zobrazit podrobně provedené akce';
+$lang['Error'] = 'Chyba';
+$lang['Error(s)'] = 'Chyby';
+$lang['Manage caddie\'s photos'] = 'Spravovat fotky v košíku';
+$lang['No album selected'] = 'Není vybráno album';
+$lang['No file selected'] = 'Není vybrán soubor';
+$lang['Not added image'] = 'Nepřidán obrázek';
+$lang['Not updated image'] = 'Neaktualizovaný obrázek';
+$lang['Save'] = 'Uložit';
+$lang['Select Album to be updated'] = 'Vyberte album které bude aktualizováno';
+$lang['Selected Album'] = 'Vybrané album';
+$lang['Selected Files'] = 'Vybrané soubory';
+$lang['Unsupported file extension'] = 'Nepodporované typy souborů';
+$lang['Update'] = 'Aktualizovat';
+$lang['Update Album'] = 'Aktualizovat album';

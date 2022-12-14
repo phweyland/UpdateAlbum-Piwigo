a55bc166a322cd1b842393116c76d3e2bc936c86
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-14 06:57:10 +0100

    Update sk_SK, thanks to dodo

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Update existing images'] = 'Aktualizujte existujúce obrázky';
+$lang['Updated image'] = 'Aktualizovaný obrázok';
+$lang['Updated images'] = 'Aktualizované obrázky';
+$lang['Add new images to caddie'] = 'Pridajte nové obrázky do súboru';
+$lang['Added image'] = 'Pridaný obrázok';
+$lang['Allowed file types'] = 'Povolené typy súborov';
+$lang['Create images if not exist'] = 'Vytvorte obrázky, ak neexistujú';
+$lang['Created images'] = 'Vytvorené obrázky';
+$lang['Display detailed actions done'] = 'Zobraziť podrobné vykonané akcie';
+$lang['Error'] = 'Chyba';
+$lang['Error(s)'] = 'chyba(y)';
+$lang['Manage caddie\'s photos'] = 'Spravujte fotografie v súbore';
+$lang['No album selected'] = 'Nie je vybratý žiadny album';
+$lang['No file selected'] = 'Nie je zvolený žiadny súbor';
+$lang['Not added image'] = 'Nepridaný obrázok';
+$lang['Not updated image'] = 'Neaktualizovaný obrázok';
+$lang['Save'] = 'Uložiť';
+$lang['Select Album to be updated'] = 'Vyberte album, ktorý chcete aktualizovať';
+$lang['Selected Album'] = 'Vybratý album';
+$lang['Selected Files'] = 'Vybrané súbory';
+$lang['Unsupported file extension'] = 'Nepodporovaná prípona súboru';
+$lang['Update'] = 'Aktualizovať';
+$lang['Update Album'] = 'Aktualizovať album';

82db6291c4b0f32cb10d642cbc676774c545c9b9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-01-24 19:15:09 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['No album selected'] = 'Nessun album selezionato.';
+$lang['No file selected'] = 'Nessun file selezionato.';
+$lang['Not added image'] = 'Nessuna immagine aggiunta.';
+$lang['Not updated image'] = 'Nessuna immagine aggiornata.';
+$lang['Save'] = 'Salva.';
+$lang['Select Album to be updated'] = 'Seleziona album da aggiornare.';
+$lang['Selected Album'] = 'Album selezionato.';
+$lang['Selected Files'] = 'File Selezionati.';
+$lang['Unsupported file extension'] = 'Tipo di file non supportato.';
+$lang['Update'] = 'Aggiorna.';
+$lang['Update Album'] = 'Aggiorna album.';
+$lang['Update existing images'] = 'Aggiorna immagini esistenti.';
+$lang['Updated image'] = 'Immagine aggiornata.';
+$lang['Updated images'] = 'Immagini aggiornate.';
+$lang['Add new images to caddie'] = 'Aggiungi nuove immagine al carrello.';
+$lang['Manage caddie\'s photos'] = 'Gestisci le fotografie nel carrello.';
+$lang['Added image'] = 'Immagine aggiunta.';
+$lang['Allowed file types'] = 'Tipi di file permessi.';
+$lang['Create images if not exist'] = 'Crea l\'immagine se non esiste già.';
+$lang['Created images'] = 'Immagini create.';
+$lang['Display detailed actions done'] = 'Mostra azioni dettagliate attivato.';
+$lang['Error'] = 'Errore.';
+$lang['Error(s)'] = 'Errori.';

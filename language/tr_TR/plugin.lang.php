be2806c590eec0c4090d3d807d136a46805ed64d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-02-04 05:45:48 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display detailed actions done'] = 'Gerçekleştirilen eylemleri ayrıntılı olarak görüntüleyin';
+$lang['Create images if not exist'] = 'Eğer yoksa imaj oluşturun';
+$lang['Manage caddie\'s photos'] = 'Sepetteki resimleri yönet';
+$lang['Add new images to caddie'] = 'Sepete yeni resimler ekle';
+$lang['Added image'] = 'Eklenen resim';
+$lang['Allowed file types'] = 'İzin verilen dosya tipleri';
+$lang['Created images'] = 'Oluşturulan resim(ler)';
+$lang['Not updated image'] = 'Güncellenmemiş resim';
+$lang['Not added image'] = 'Eklenmemiş resim';
+$lang['Select Album to be updated'] = 'Güncellenecek Albümü Seçin';
+$lang['Update Album'] = 'Albümü Güncelle';
+$lang['Update'] = 'Güncelle';
+$lang['Unsupported file extension'] = 'Desteklenmeyen dosya uzantısı';
+$lang['Selected Files'] = 'Seçilen Dosya(lar)';
+$lang['Selected Album'] = 'Seçilen Albüm';
+$lang['Save'] = 'Kaydet';
+$lang['No file selected'] = 'Hiç dosya seçilmedi';
+$lang['No album selected'] = 'Hiç albüm seçilmedi';
+$lang['Error(s)'] = 'Hata(lar)';
+$lang['Error'] = 'Hata';
+$lang['Update existing images'] = 'Mevcut resimleri güncelle';
+$lang['Updated image'] = 'Güncellenen resim';
+$lang['Updated images'] = 'Güncellenen resim(ler)';

41f8caf986db68b139ee162fa7b72806dd518a79
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-08 17:22:57 +0200

    Update ru_RU, thanks to rurio

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Add new images to caddie'] = 'Добавляйте новые изображения в кэдди';
+$lang['Added image'] = 'Добавлено изображение';
+$lang['Allowed file types'] = 'Разрешенные типы файлов';
+$lang['Create images if not exist'] = 'Создать изображения, если они не существуют';
+$lang['Created images'] = 'Созданное(ые) изображение(я)';
+$lang['Display detailed actions done'] = 'Показать подробные действия, которые были выполнены';
+$lang['Error'] = 'Ошибка';
+$lang['Error(s)'] = 'Ошибка(и)';
+$lang['Manage caddie\'s photos'] = 'Управление фотографиями Кэдди';
+$lang['No album selected'] = 'Альбом не выбран';
+$lang['No file selected'] = 'Файл не выбран';
+$lang['Not added image'] = 'Изображение не добавлено';
+$lang['Not updated image'] = 'Не обновленное изображение';
+$lang['Save'] = 'Сохранить';
+$lang['Select Album to be updated'] = 'Выберите альбом для обновления';
+$lang['Selected Album'] = 'Выбранный альбом';
+$lang['Selected Files'] = 'Выбранные файлы';
+$lang['Unsupported file extension'] = 'Неподдерживаемое расширение файла';
+$lang['Update'] = 'Обновить';
+$lang['Update Album'] = 'Обновить альбом';
+$lang['Update existing images'] = 'Обновите существующие изображения';
+$lang['Updated image'] = 'Обновленное изображение';
+$lang['Updated images'] = 'Обновленные изображения';

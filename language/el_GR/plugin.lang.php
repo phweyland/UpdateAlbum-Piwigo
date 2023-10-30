ec49937d8e0eb18f74512441119f366b0afd1033
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-10-30 07:27:30 +0100

    Update el_GR, thanks to bas_alba

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Allowed file types'] = 'Επιτρεπόμενοι τύποι αρχείων';
+$lang['Create images if not exist'] = 'Δημιουργήστε εικόνες εάν δεν υπάρχουν';
+$lang['Display detailed actions done'] = 'Εμφάνιση λεπτομερών ενεργειών που έγιναν';
+$lang['Created images'] = 'Δημιουργήθηκαν εικόνες';
+$lang['Added image'] = 'Προστέθηκε εικόνα';
+$lang['Add new images to caddie'] = 'Προσθέστε νέες εικόνες στο καλάθι';

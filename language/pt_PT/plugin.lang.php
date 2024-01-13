687e3a79df97cbcc47c19ecb0ec8865b6ab17942
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-01-13 13:23:09 +0100

    Update pt_PT, thanks to ANO

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Update existing images'] = 'Atualizar as imagens existentes';
+$lang['Updated image'] = 'Atualizar imagem';
+$lang['Updated images'] = 'Atualizar imagem (ns)';
+$lang['Add new images to caddie'] = 'Adicionar nova imagem ao caddie';
+$lang['Added image'] = 'Adicionar imagem';
+$lang['Allowed file types'] = 'Permitido tipos de arquivos';
+$lang['Create images if not exist'] = 'Criar imagens se não existem';
+$lang['Created images'] = 'imagen(s) criada(s)';
+$lang['Display detailed actions done'] = 'Exibir ações detalhadas realizadas';
+$lang['Error'] = 'Erro';
+$lang['Error(s)'] = 'Erro(s)';
+$lang['Manage caddie\'s photos'] = 'Gerencie as fotos do caddie';
+$lang['No album selected'] = 'Album não selecionado';
+$lang['No file selected'] = 'Arquivo não selecionado';
+$lang['Not added image'] = 'Imagem não adicionada';
+$lang['Not updated image'] = 'Imagem não atualizada';
+$lang['Save'] = 'Salvar';
+$lang['Select Album to be updated'] = 'Selecionar album para ser atualizado';
+$lang['Selected Album'] = 'Album selecionado';
+$lang['Selected Files'] = 'Arquivos selecionados';
+$lang['Unsupported file extension'] = 'Extensão de arquivo não suportada';
+$lang['Update'] = 'Atualizar';
+$lang['Update Album'] = 'Atualizar album';

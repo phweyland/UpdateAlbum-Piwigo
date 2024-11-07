353ba61fdcfbaf1ab80ce8cad4acfd383f8aa76f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-07 05:55:41 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Update existing images'] = '更新现有图像';
+$lang['Updated image'] = '已更新图像';
+$lang['Updated images'] = '已更新图像';
+$lang['Add new images to caddie'] = '将新图像添加到临时篮';
+$lang['Added image'] = '已添加图像';
+$lang['Allowed file types'] = '允许的文件类型';
+$lang['Create images if not exist'] = '如果图像不存在则创建图像';
+$lang['Created images'] = '已创建图像';
+$lang['Display detailed actions done'] = '显示已完成的详细操作';
+$lang['Error'] = '错误';
+$lang['Error(s)'] = '错误';
+$lang['Manage caddie\'s photos'] = '管理临时篮中的照片';
+$lang['No album selected'] = '未选择相册';
+$lang['No file selected'] = '未选择文件';
+$lang['Not added image'] = '未添加图像';
+$lang['Not updated image'] = '未更新图像';
+$lang['Save'] = '保存';
+$lang['Select Album to be updated'] = '选择要更新的相册';
+$lang['Selected Album'] = '选择相册';
+$lang['Selected Files'] = '选择文件';
+$lang['Unsupported file extension'] = '不支持的文件扩展名';
+$lang['Update'] = '更新';
+$lang['Update Album'] = '更新相册';

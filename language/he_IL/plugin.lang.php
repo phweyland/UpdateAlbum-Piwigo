ed5203156956afc07a697be50dd1e70a012bcff4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 22:49:20 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Add new images to caddie'] = 'הוסף תמונות חדשות לעגלה';
+$lang['Added image'] = 'תמונה נוספת';
+$lang['Allowed file types'] = 'סוגי קבצים מורשים';
+$lang['Create images if not exist'] = 'צור תמונות אם לא קיימות';
+$lang['Created images'] = 'תמונה/ות נוצרו';
+$lang['Display detailed actions done'] = 'הצג פעולות מפורטות שבוצעו';
+$lang['Error'] = 'שגיאה';
+$lang['Error(s)'] = 'שגיאות';
+$lang['Manage caddie\'s photos'] = 'נהל את התמונות בעגלה';
+$lang['No album selected'] = 'לא נבחר אלבום';
+$lang['No file selected'] = 'לא נבחר קובץ';
+$lang['Not added image'] = 'לא הוסף תמונה';
+$lang['Not updated image'] = 'לא עודכן תמונה';
+$lang['Save'] = 'שמור';
+$lang['Select Album to be updated'] = 'בחר אלבום לעדכון';
+$lang['Selected Album'] = 'אלבום נבחר';
+$lang['Selected Files'] = 'קובץ/קבצים נבחרים';
+$lang['Unsupported file extension'] = 'סיומת קובץ לא נתמכת';
+$lang['Update'] = 'עדכן';
+$lang['Update Album'] = 'עדכן אלבום';
+$lang['Update existing images'] = 'עדכן תמונות קיימות';
+$lang['Updated image'] = 'תמונה עודכנה';
+$lang['Updated images'] = 'תמונה/ות עודכנו';

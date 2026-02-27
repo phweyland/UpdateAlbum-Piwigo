0f19f4b035e2ded9caf9af15c74d91b6b6197f27
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-27 04:01:29 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Update existing images'] = '기존 이미지 업데이트';
+$lang['Updated image'] = '이미지 업데이트됨';
+$lang['Updated images'] = '이미지 업데이트됨';
+$lang['Add new images to caddie'] = '캐디에 새 이미지 추가';
+$lang['Added image'] = '이미지 추가됨';
+$lang['Allowed file types'] = '허용되는 파일 형식';
+$lang['Create images if not exist'] = '이미지가 없을 경우 생성';
+$lang['Created images'] = '이미지 생성됨';
+$lang['Display detailed actions done'] = '실행된 세부 작업 표시';
+$lang['Error'] = '오류';
+$lang['Error(s)'] = '오류';
+$lang['Manage caddie\'s photos'] = '캐디 사진 관리';
+$lang['No album selected'] = '앨범 선택되지 않음';
+$lang['No file selected'] = '파일 선택되지 않음';
+$lang['Not added image'] = '이미지 추가되지 않음';
+$lang['Not updated image'] = '이미지 업데이트되지 않음';
+$lang['Save'] = '저장';
+$lang['Select Album to be updated'] = '업데이트할 앨범 선택';
+$lang['Selected Album'] = '선택된 앨범';
+$lang['Selected Files'] = '선택된 파일';
+$lang['Unsupported file extension'] = '지원되지 않는 파일 확장자';
+$lang['Update'] = '업데이트';
+$lang['Update Album'] = '앨범 업데이트';

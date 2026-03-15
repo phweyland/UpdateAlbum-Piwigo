ef3f8dfea48e8b697c3411de25561838fe417f46
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-03-15 06:31:19 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Add new images to caddie'] = 'Afegeix imatges noves al carret';
+$lang['Added image'] = 'Imatge afegida';
+$lang['Allowed file types'] = 'Tipus de fitxer permesos';
+$lang['Create images if not exist'] = 'Crea les imatges si no existeixen';
+$lang['Created images'] = 'Imatge(s) creade(s)';
+$lang['Display detailed actions done'] = 'Mostra en detall les accions realitzades';
+$lang['Error'] = 'Error';
+$lang['Error(s)'] = 'Error(s)';
+$lang['Manage caddie\'s photos'] = 'Gestiona les fotos del carret';
+$lang['No album selected'] = 'No s\'ha seleccionat cap àlbum';
+$lang['No file selected'] = 'No s\'ha seleccionat cap fitxer';
+$lang['Not added image'] = 'No s\'ha afegit cap imatge';
+$lang['Not updated image'] = 'Imatge no actualitzada';
+$lang['Save'] = 'Desa';
+$lang['Select Album to be updated'] = 'Seleccioneu l\'àlbum que s\'ha d\'actualitzar';
+$lang['Selected Album'] = 'Àlbum seleccionat';
+$lang['Selected Files'] = 'Fitxer(s) seleccionat(s)';
+$lang['Unsupported file extension'] = 'Extensió de fitxer no suportada';
+$lang['Update'] = 'Actualitza';
+$lang['Update Album'] = 'Actualitza l\'àlbum';
+$lang['Update existing images'] = 'Actualitza les imatges existents';
+$lang['Updated image'] = 'Imatge actualitzada';
+$lang['Updated images'] = 'Imatge(s) actualitzada(des)';
